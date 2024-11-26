04b232d536787923b94d3ea2d2e68d9bc524c82b
nico-best1 <nicobest@ucm.es>
2024-11-26 20:43:01 +0100

    creacion sistema particuls fire en .particle

--- ProyectosOGREvc15x86/IG2App/EmisoresParticulas.h
+++ ProyectosOGREvc15x86/IG2App/EmisoresParticulas.h
@@ -6,7 +6,7 @@ class EmisoresParticulas : public IG2Object
 public:
     EmisoresParticulas(Vector3 pos, SceneNode* node, SceneManager* sceneManager, int n_particle_system_bloques)
         : IG2Object(pos, node, sceneManager) {
-        ParticleSystem* pSys = mSM->createParticleSystem("pslaberintoSmoke" + n_particle_system_bloques, "example/laberintoSmoke");
+        ParticleSystem* pSys = mSM->createParticleSystem("pslaberintoSmoke" + n_particle_system_bloques, "laberintoSmoke");
         pSys->setEmitting(true);
         mNode->attachObject(pSys);
     }
--- ProyectosOGREvc15x86/IG2App/VillanoAnimation.cpp
+++ ProyectosOGREvc15x86/IG2App/VillanoAnimation.cpp
@@ -2,7 +2,7 @@
 VillanoAnimation::VillanoAnimation(Vector3 pos, SceneNode* node, SceneManager* sceneManager, String mesh) :
     Personaje(pos, node, sceneManager, mesh), keyframePos(pos){
     setScale(Vector3(0.8, 0.8, 0.8));
-	ParticleSystem* pSys = mSM->createParticleSystem("psSmoke", "example/villanoSmoke");
+	ParticleSystem* pSys = mSM->createParticleSystem("psSmokeVillano", "villanoSmoke");
 	pSys->setEmitting(true);
 	mNode->attachObject(pSys);
     createAnimationVillanoWalk();
