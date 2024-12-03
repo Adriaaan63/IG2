42bb5c32db53150e5d5373baaf22005328e3a66c
nico-best1 <nicobest@ucm.es>
2024-12-03 21:34:15 +0100

    Revert "ajustes parametros finales"
    This reverts commit 709f52271ec784fb4ec8b5d4aadbc1f41b6943bd.

--- ProyectosOGREvc15x86/media/IG2App/SpaceSkyZoomLightFS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/SpaceSkyZoomLightFS.glsl
@@ -1,27 +1,32 @@
 #version 330 core
-uniform sampler2D texturaL; 
-uniform sampler2D texturaM;  
-uniform float BF;            
+uniform sampler2D texturaL;  // Textura 1
+uniform sampler2D texturaM;  // Textura 2
+
+uniform float BF;            // Factor de mezcla (Blending factor)
 
 uniform float minL;
 uniform float maxL;
 
-in vec2 vUv0;                
-in float zf;                 
+in vec2 vUv0;                // Coordenadas UV modificadas desde el vertex shader
+in float zf;                 // Factor de zoom desde el vertex shader
 in float tiempo;
 
-out vec4 fFragColor;         
+out vec4 fFragColor;         // Color final del fragmento
 
 void main() {
+    // Obtener los colores de las texturas usando las coordenadas UV
     vec3 colorL = vec3(texture(texturaL, vUv0));
     vec3 colorM = vec3(texture(texturaM, vUv0));
 
+    // Mezclar los colores de las texturas usando el blending factor
     vec3 color = mix(colorL, colorM, BF);
 
     float diff = maxL - minL;
     float lf = minL + (diff*((tiempo + 1) / 2));
 
+    // Aplicar el factor de zoom y la luz
     color *= zf * lf;
 
+    // Establecer el color final del fragmento
     fFragColor = vec4(color, 1.0);
 }
