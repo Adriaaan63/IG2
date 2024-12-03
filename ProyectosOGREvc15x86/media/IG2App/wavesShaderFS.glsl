709f52271ec784fb4ec8b5d4aadbc1f41b6943bd
Nicolás Besteiro <nicobest@ucm.es>
2024-12-03 14:21:54 +0100

    ajustes parametros finales

--- ProyectosOGREvc15x86/media/IG2App/wavesShaderFS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/wavesShaderFS.glsl
@@ -5,7 +5,6 @@ out vec4 fFragColor;
 
 
 void main() {
-
     vec3 colorL = vec3(texture(texturaL, vUv0));
     fFragColor = vec4(colorL, 1.0);
 }
--- ProyectosOGREvc15x86/media/IG2App/wavesShaderVS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/wavesShaderVS.glsl
@@ -9,7 +9,7 @@ out vec2 vUv0;
 void main() {
     vUv0 = uv0;
     vec4 vertexCoord = vertex;
-     float distanceCenter = length(vertex.xyz - centro.xyz);
+    float distanceCenter = length(vertex.xyz - centro.xyz);
     vertexCoord.y += sin(vertexCoord.x + (time*0.25)) * 1.0 + sin(vertexCoord.z + (distanceCenter) + (time*25)) * 1.0; 
     gl_Position = modelViewProjMat * vertexCoord;
 }
